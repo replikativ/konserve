2c3ab0f5755ddae91f072617f251cb1599cbacab
Christian Weilbach <christian_weilbach@web.de>
2014-04-28 00:40:39 +0200

    bump clojure and cljs versions.

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj" "src/cljs"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [com.ashafa/clutch "0.4.0-RC1"]]
 
-  :plugins [[lein-cljsbuild "1.0.1"]
+  :plugins [[lein-cljsbuild "1.0.3"]
             [com.keminglabs/cljx "0.3.2"]
-            [com.cemerick/austin "0.1.3"]]
+            [com.cemerick/austin "0.1.4"]]
 
   :cljx {:builds [{:source-paths ["src/cljx"]
                    :output-path "target/classes"
