22f2d7ac365363049a4f0ed9f336a037d0a25a7b
Alexander Oloo <alekcz@gmail.com>
2020-08-29 12:42:26 +0200

    rename layouts

--- src/konserve/storage_layout.clj
+++ src/konserve/storage_layout.clj
@@ -3,11 +3,14 @@
   access depending on the low-level storage layout chosen. Stores can support
   multiple layouts.")
 
-(defprotocol Layout1
+(defprotocol LinearLayout
+  ;; Location 1: [4-header-bytes 4-bytes-for-meta-size serialized-meta serialized-data]
   (-get-raw [store key])
   (-put-raw [store key blob]))
 
-(defprotocol Layout2
+(defprotocol SplitLayout
+  ;; Location 1: [4-header-bytes serialized-meta]
+  ;; Location 2: [4-header-bytes serialized-data]
   (-get-raw-meta [store key])
   (-put-raw-meta [store key blob])
   (-get-raw-value [store key])
