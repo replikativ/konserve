523e10f554882fc74e66e9579b1d57077dc365b7
Judith Massa <j.massa@lambdaforge.io>
2023-01-23 20:21:04 +0100

    Add clj kondo config

---
+++ .clj-kondo/hooks/go_try_.clj
@@ -0,0 +1,11 @@
+(ns hooks.go-try-
+  (:require [clj-kondo.hooks-api :as api]))
+
+(defn go-try- [{:keys [:node]}]
+  (let [new-node (api/list-node
+                  (apply list
+                         (concat
+                          [(api/token-node 'try)]
+                          (rest (:children node))
+                          [(api/list-node (list (api/token-node 'finally)))])))]
+    {:node new-node}))
