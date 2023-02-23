e2c1cb45708006a62a1df2133261620a2b70c3c8
Timo Kramer <4785848+TimoKramer@users.noreply.github.com>
2023-02-23 15:46:04 +0100

    ci: simplify pipeline (#95)
    * ci: simplified pipeline
    
    * fixup! ci: simplified pipeline
    
    * fixup! ci: simplified pipeline
    
    * fixup! ci: simplified pipeline
    
    * fixup! ci: simplified pipeline
    
    * fixup! ci: simplified pipeline
    
    * fixup! ci: simplified pipeline
    
    * fixup! ci: simplified pipeline

--- build.clj
+++ build.clj
@@ -2,9 +2,12 @@
   (:refer-clojure :exclude [test])
   (:require [clojure.tools.build.api :as b]
             [borkdude.gh-release-artifact :as gh]
-            [org.corfield.build :as bb]))
+            [deps-deploy.deps-deploy :as dd])
+  (:import [clojure.lang ExceptionInfo]))
 
+(def org "replikativ")
 (def lib 'io.replikativ/konserve)
+(def current-commit (b/git-process {:git-args "rev-parse HEAD"}))
 (def version (format "0.7.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
@@ -15,41 +18,65 @@
   (b/delete {:path "target"}))
 
 (defn jar
-  [opts]
-  (-> opts
-      (assoc :class-dir class-dir
-             :src-pom "./template/pom.xml"
-             :lib lib
-             :version version
-             :basis basis
-             :jar-file jar-file
-             :src-dirs ["src"])
-      bb/jar))
+  [_]
+  (b/write-pom {:class-dir class-dir
+                :src-pom "./template/pom.xml"
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
 
-(defn ci "Run the CI pipeline of tests (and build the JAR)." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/clean)
-      (bb/jar)))
+(defn deploy
+  "Don't forget to set CLOJARS_USERNAME and CLOJARS_PASSWORD env vars."
+  [_]
+  (dd/deploy {:installer :remote :artifact jar-file
+              :pom-file (b/pom-path {:lib lib :class-dir class-dir})}))
 
-(defn install "Install the JAR locally." [opts]
-  (-> opts
-      jar
-      bb/install))
+(defn fib [a b]
+  (lazy-seq (cons a (fib b (+ a b)))))
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
-  (-> opts
-      (assoc :lib lib :version version)
-      (bb/deploy)))
+(defn retry-with-fib-backoff [retries exec-fn test-fn]
+  (loop [idle-times (take retries (fib 1 2))]
+    (let [result (exec-fn)]
+      (if (test-fn result)
+        (do (println "Returned: " result)
+            (if-let [sleep-ms (first idle-times)]
+              (do (println "Retrying with remaining back-off times (in s): " idle-times)
+                  (Thread/sleep (* 1000 sleep-ms))
+                  (recur (rest idle-times)))
+              result))
+        result))))
+
+(defn try-release []
+  (try (gh/overwrite-asset {:org org
+                            :repo (name lib)
+                            :tag version
+                            :commit current-commit
+                            :file jar-file
+                            :content-type "application/java-archive"
+                            :draft false})
+       (catch ExceptionInfo e
+         (assoc (ex-data e) :failure? true))))
 
 (defn release
   [_]
-  (-> (gh/overwrite-asset {:org "replikativ"
-                           :repo (name lib)
-                           :tag version
-                           :commit (gh/current-commit)
-                           :file jar-file
-                           :content-type "application/java-archive"})
-      :url
-      println))
+  (println "Trying to release artifact...")
+  (let [ret (retry-with-fib-backoff 10 try-release :failure?)]
+    (if (:failure? ret)
+      (do (println "GitHub release failed!")
+          (System/exit 1))
+      (println (:url ret)))))
 
+(defn install
+  [_]
+  (clean nil)
+  (jar nil)
+  (b/install {:basis (b/create-basis {})
+              :lib lib
+              :version version
+              :jar-file jar-file
+              :class-dir class-dir}))
