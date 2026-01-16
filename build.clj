19b01da349a89fa31e21271deb877f9306935fd7
Christian Weilbach <christian@weilbach.name>
2026-01-16 00:00:34 -0800

    Migrate to org.replikativ namespace
    - Update artifact: io.replikativ/konserve -> org.replikativ/konserve
    - Update dependencies to org.replikativ versions:
      - hasch 0.3.94 -> 0.3.96
      - incognito 0.3.66 -> 0.3.69
      - superv.async 0.3.46 -> 0.3.50
      - geheimnis 0.1.1 -> 0.1.25
    - Update README badges and examples
    - Tests passing: 57 tests, 1071 assertions

--- build.clj
+++ build.clj
@@ -6,7 +6,7 @@
   (:import [clojure.lang ExceptionInfo]))
 
 (def org "replikativ")
-(def lib 'io.replikativ/konserve)
+(def lib 'org.replikativ/konserve)
 (def current-commit (b/git-process {:git-args "rev-parse HEAD"}))
 (def version (format "0.9.%s" (b/git-count-revs nil)))
 (def class-dir "target/classes")
